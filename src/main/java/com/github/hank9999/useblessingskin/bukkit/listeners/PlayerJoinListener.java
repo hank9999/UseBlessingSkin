9875fc6100f474987f9e1d359931eea2cfc7a5cd
hank9999 <hank9999@qq.com>
2024-07-25 02:21:37 +0800

    refactor: fewer nesting

--- src/main/java/com/github/hank9999/useblessingskin/bukkit/listeners/PlayerJoinListener.java
+++ src/main/java/com/github/hank9999/useblessingskin/bukkit/listeners/PlayerJoinListener.java
@@ -15,32 +15,33 @@ import java.util.Optional;
 public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (GetConfig.checkPath("loginAutoSetSkin") && GetConfig.bool("loginAutoSetSkin")) {
-            Player p = event.getPlayer();
-            Optional<SkinIdentifier> skinId = UseBlessingSkin.playerStorage.getSkinIdOfPlayer(p.getUniqueId());
-
-            if (skinId.isPresent()) {
-                return;
-            }
-
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    try {
-                        if (!GetConfig.checkPath("loginAutoSetSkinMojangFirst") || GetConfig.bool("loginAutoSetSkinMojangFirst")) {
-
-                            if (UseBlessingSkin.mojangAPI.getSkin(p.getName()).isPresent()) {
-                                return;
-                            }
-                            SkinSetter.setSkin(p.getName(), p);
-
-                        } else {
-                            SkinSetter.setSkin(p.getName(), p);
-                        }
-                    } catch (Exception ignored) {
-                    }
-                }
-            }.runTaskAsynchronously(UseBlessingSkin.plugin);
+        if (!GetConfig.checkPath("loginAutoSetSkin") || !GetConfig.bool("loginAutoSetSkin")) {
+            return;
         }
+        Player p = event.getPlayer();
+        Optional<SkinIdentifier> skinId = UseBlessingSkin.playerStorage.getSkinIdOfPlayer(p.getUniqueId());
+
+        if (skinId.isPresent()) {
+            return;
+        }
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    if (!GetConfig.checkPath("loginAutoSetSkinMojangFirst") || GetConfig.bool("loginAutoSetSkinMojangFirst")) {
+
+                        if (UseBlessingSkin.mojangAPI.getSkin(p.getName()).isPresent()) {
+                            return;
+                        }
+                        SkinSetter.setSkin(p.getName(), p);
+
+                    } else {
+                        SkinSetter.setSkin(p.getName(), p);
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        }.runTaskAsynchronously(UseBlessingSkin.plugin);
     }
 }
--- src/main/java/com/github/hank9999/useblessingskin/bungee/listeners/ServerConnectListener.java
+++ src/main/java/com/github/hank9999/useblessingskin/bungee/listeners/ServerConnectListener.java
@@ -1,7 +1,7 @@
 package com.github.hank9999.useblessingskin.bungee.listeners;
 
-import com.github.hank9999.useblessingskin.bungee.libs.GetConfig;
 import com.github.hank9999.useblessingskin.bungee.UseBlessingSkin;
+import com.github.hank9999.useblessingskin.bungee.libs.GetConfig;
 import com.github.hank9999.useblessingskin.bungee.libs.SkinSetter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectEvent;
@@ -14,27 +14,28 @@ import java.util.Optional;
 public class ServerConnectListener implements Listener {
     @EventHandler
     public void onServerConnect(ServerConnectEvent event) {
-        if (GetConfig.checkPath("loginAutoSetSkin") && GetConfig.bool("loginAutoSetSkin")) {
-            ProxiedPlayer p = event.getPlayer();
-            Optional<SkinIdentifier> skinId = UseBlessingSkin.playerStorage.getSkinIdOfPlayer(p.getUniqueId());
-
-            if (skinId.isPresent()) {
-                return;
-            }
-
-            UseBlessingSkin.instance.getProxy().getScheduler().runAsync(UseBlessingSkin.instance, (() -> {
-                try {
-                    if (!GetConfig.checkPath("loginAutoSetSkinMojangFirst") || GetConfig.bool("loginAutoSetSkinMojangFirst")) {
-                        if (UseBlessingSkin.mojangAPI.getSkin(p.getName()).isPresent()) {
-                            return;
-                        }
-                        SkinSetter.setSkin(p.getName(), p);
-                    } else {
-                        SkinSetter.setSkin(p.getName(), p);
-                    }
-                } catch (Exception ignored) {
-                }
-            }));
+        if (!GetConfig.checkPath("loginAutoSetSkin") || !GetConfig.bool("loginAutoSetSkin")) {
+            return;
         }
+        ProxiedPlayer p = event.getPlayer();
+        Optional<SkinIdentifier> skinId = UseBlessingSkin.playerStorage.getSkinIdOfPlayer(p.getUniqueId());
+
+        if (skinId.isPresent()) {
+            return;
+        }
+
+        UseBlessingSkin.instance.getProxy().getScheduler().runAsync(UseBlessingSkin.instance, (() -> {
+            try {
+                if (!GetConfig.checkPath("loginAutoSetSkinMojangFirst") || GetConfig.bool("loginAutoSetSkinMojangFirst")) {
+                    if (UseBlessingSkin.mojangAPI.getSkin(p.getName()).isPresent()) {
+                        return;
+                    }
+                    SkinSetter.setSkin(p.getName(), p);
+                } else {
+                    SkinSetter.setSkin(p.getName(), p);
+                }
+            } catch (Exception ignored) {
+            }
+        }));
     }
 }
